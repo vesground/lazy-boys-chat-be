50c63441f4393d4bb3d6a40021b0119eaa5e9b49
Kiril Stas <vesground@gmail.com>
2019-02-23 11:22:09 +0200

    Added global error handler

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import bodyParser from 'body-parser';
 import config from 'config/config.json';
 import logger from 'config/logger.js'
 import routes from 'lib/api/routes';
+import { InternalError } from 'lib/api/services/Exception';
 
 const { host, port } = config;
 const app = express();
@@ -17,7 +18,16 @@ app.use(morgan('combined', { stream: logger.stream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function timeLog(req, res, next) {
+  console.log(new Date(), `${req.method} ${req.path}`);
+  next();
+});
+
 app.use('/api/v1', routes);
+
+app.use(function(error, req, res, next) {
+  res.json(new InternalError(error));
+});
 
 app.listen(port, host);
 
