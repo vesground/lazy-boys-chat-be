b8f60f883cfb6a59c9953ca96faa8891baa72b93
Kiril Stas <vesground@gmail.com>
2019-03-15 13:08:15 +0200

    Added test to check error while creating user with the same name

--- test/users/create.js
+++ test/users/create.js
@@ -1,7 +1,8 @@
 import { expect } from 'chai';
+import moment from 'moment';
 import { baseUrl } from 'test/config.js';
 import { post } from 'test/ApiClient';
-import { BadRequestCreator } from 'lib/Exception';
+import { BadRequestCreator, InternalErrorCreator } from 'lib/Exception';
 
 const route = '/api/v1/users';
 const url = `${baseUrl}${route}`
@@ -35,23 +36,25 @@ describe(`POST ${url}`, function() {
   })
 
   context('With params', function() {
-    it('should return a correct response on request', async () => {
+    it('should return a successfuly created new user', async () => {
       const res = await post(url, data);
-      const correctResponse = Object.assign(data, { status: 200 });
+      const timestampNow = moment().format('X');
+      const correctResponse = Object.assign(data, { id: 'ObjectId(1)', createdAt: timestampNow, updatedAt: timestampNow });
 
       expect(res.status).to.equal(200);
-      expect(res.data).to.equal(data.data);
+      expect(JSON.stringify(res.data)).to.equal(JSON.stringify(data));
     })
   })
 
-  // context('With params', function() {
-  //   it('should return an error cause user with the same name already exist', async () => {
-  //     const res = await post(url, data);
-  //     const correctResponse = Object.assign(data, { status: 200 });
-  //
-  //     expect(res.status).to.equal(200);
-  //     expect(res.data).to.equal(data.data);
-  //   })
-  // })
+  context('With params', function() {
+    it('should return an error cause user with the same name already exist', async () => {
+      data.name = 'ohmime';
+      const res = await post(url, data);
+      const error = InternalErrorCreator(`User with name ${data.name} already exist!`);
+
+      expect(res.status).to.equal(error.status);
+      expect(res.message).to.equal(error.message);
+    })
+  })
 
 })
